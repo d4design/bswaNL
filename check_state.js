a55062fb94fd0727544e93b6b330aee70b808742
d4design <32513583+d4design@users.noreply.github.com>
2018-11-28 08:33:33 +1100

    Check state and get stats added

--- check_state.js
+++ check_state.js
@@ -1,7 +1,7 @@
 var SerialPort = require('serialport');
 var fs = require('fs');
 
-const QUERY_STATS = Buffer.from([0x02, 0x01, 0x43, 0x53, 0x54, 0x53, 0x3F, 0x03, 0x28, 0x0D, 0x0A]);
+const QUERY_STATE = Buffer.from([0x02, 0x01, 0x43, 0x53, 0x54, 0x41, 0x3F, 0x03, 0x3A, 0x0D, 0x0A]);
 
 let dataBuffer = [];
 
@@ -28,7 +28,7 @@ port.on('error', function(err) {
   console.log('Error: ', err.message);
 });
 
-function processStats(data) {
+function processData(data) {
   //Remove device id
   data.splice(0, 1);
   //Remove the CR
@@ -40,48 +40,16 @@ function processStats(data) {
   // Get the ATTR ATTR_RESPONSE
   const command = data.splice(0, 1)[0];  //should be 0x41 which is the Response Block at start of Response data
 
-  let statsString = '';
+  let dataString = '';
   for (var i = 0; i < data.length; i++) {
-    statsString += String.fromCharCode(data[i]);
+    dataString += String.fromCharCode(data[i]);
   }
 
-  var statsArray = statsString.split(',').map(Number);
+  var dataArray = dataString.split(',').map(Number);
 
-  //translate frequency weightings
-  switch (statsArray[0]) {
-    case 0:
-      statsArray[0] = 'LA';
-      break;
-    case 1:
-      statsArray[0] = 'LB';
-      break;
-    case 2:
-      statsArray[0] = 'LC';
-      break;
-    case 3:
-      statsArray[0] = 'LZ';
-      break;
-    default:
-      statsArray[0] = 'Error';
-  }
+  console.log(dataArray);
 
-  //translate detector settings
-  switch (statsArray[1]) {
-    case 0:
-      statsArray[1] = 'F';
-      break;
-    case 1:
-      statsArray[1] = 'S';
-      break;
-    case 2:
-      statsArray[1] = 'I';
-      break;
-    default:
-      statsArray[1] = 'Error';
-  }
-
-console.log(statsArray);
-port.close()
+  port.close();
 }
 
 let isReading = false;
@@ -90,7 +58,7 @@ port.on('data', function (data) {
     let b = data[i];
     if (isReading) {
       if (b === 0x0A && dataBuffer[dataBuffer.length-1] === 0x0D && dataBuffer[dataBuffer.length-3] === 0x03) { //Check for ending signal
-        processStats(dataBuffer);
+        processData(dataBuffer);
         isReading = false;
         dataBuffer = [];
       } else {
@@ -103,7 +71,7 @@ port.on('data', function (data) {
 
 });
 
-port.write(QUERY_STATS, function(err) {
+port.write(QUERY_STATE, function(err) {
   if (err) {
     return console.log('Error on write: ', err.message);
   }
